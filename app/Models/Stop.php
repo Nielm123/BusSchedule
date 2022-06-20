fa538472544ad65eb19366eec1cb851a57bc81fe
fk117808 <fk117808@stud.ur.edu.pl>
2022-06-20 13:07:44 +0200

    Models and Controllers added

---
+++ app/Models/Stop.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stop extends Model
+{
+    use HasFactory;
+
+    public function stops()
+    {
+        return $this->belongsTo(Stop::class);
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function route()
+    {
+        return $this->belongsTo(Route::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/logout', function () {
+    Auth::logout();
+    return view('home');
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
