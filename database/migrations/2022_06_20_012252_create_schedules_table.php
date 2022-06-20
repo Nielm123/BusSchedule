772d611901419be0e2494ab70670fe1cf785d544
Nielm123 <nielm123@gmail.com>
2022-06-20 12:22:21 +0200

    first commit

---
+++ database/migrations/2022_06_20_012252_create_schedules_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('vehicle_id')->unsigned();
+            $table->integer('stops_id')->unsigned();
+            $table->integer('route_id')->unsigned();
+            $table->tinyInteger('day')->unsigned();
+            $table->time('time');
+
+            $table->unique([
+                'vehicle_id',
+                'stops_id',
+                'route_id',
+                'day',
+                'time'
+            ]);
+
+            $table
+                ->foreign('vehicle_id')
+                ->references('id')
+                ->on('vehicles')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('stops_id')
+                ->references('id')
+                ->on('stops')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign('route_id')
+                ->references('id')
+                ->on('routes')
+                ->onDelete('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
